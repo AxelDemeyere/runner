9539fe12d613572aa89b18609b7ab81ef0399e07
Axel Demeyere <139327423+AxelDemeyere@users.noreply.github.com>
2025-06-04 10:00:01 +0200

    PWA Implementation and Mobile Optimization

---
+++ src/app/app.config.ts
@@ -0,0 +1,27 @@
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { TokenInterceptorProvider } from './helper/token.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    JwtHelperService,
+    provideToastr(),
+    TokenInterceptorProvider,
+    importProvidersFrom(BrowserAnimationsModule),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+};
